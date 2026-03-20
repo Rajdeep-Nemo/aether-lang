ad036776f3574c316eb0692e3b0c13ba181b72b1
Rajdeep Nemo <rajdeepnemo@gmail.com>
2026-03-20 09:34:32 +0530

    arena.hpp implemented for the arena allocator

---
+++ src/Utils/arena.hpp
@@ -0,0 +1,17 @@
+#ifndef ARENA_HPP
+#define ARENA_HPP
+#include <cstddef>
+// Struct to hold the arena
+struct Arena {
+    std::byte *arena_buffer;
+    size_t arena_capacity;
+    size_t arena_offset;
+};
+// Initialize the arena
+void arena_init(Arena *arena);
+// Allocate memory for the arena
+void *arena_alloc(Arena *arena, size_t size);
+// Free the memory
+void arena_free(Arena *arena);
+
+#endif

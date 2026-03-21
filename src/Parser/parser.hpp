e261e2eb1d67d7d6b3bab56afe9c584d1297d6d5
Rajdeep Nemo <rajdeepnemo@gmail.com>
2026-03-21 18:44:10 +0530

    Basic helper functions implemented, parsing of number nd basic operators added

---
+++ src/Parser/parser.hpp
@@ -0,0 +1,27 @@
+#ifndef PARSER_HPP
+#define PARSER_HPP
+#include <vector>
+#include "token.hpp"
+#include "arena.hpp"
+#include "ast.hpp"
+// Parser blueprint
+struct Parser {
+    std::vector<Token> tokens;
+    size_t current_position;
+    Arena *arena;
+};
+// Checks the next token without consuming the current one
+Token peek(Parser *parser);
+// Function to advance to the next token
+Token advance(Parser *parser);
+// Function to check if we have reached the end of the file
+bool is_at_end(const Parser *parser);
+// Function to parse the tokens into AST
+ASTNode *parse(Parser *parser);
+// Parses a number literal (integers/floating points)
+ASTNode *parse_number(Parser *parser);
+// Parses * and /
+ASTNode *parse_multiplication_and_division(Parser *parser);
+// Parses + and -
+ASTNode *parse_addition_and_subtraction(Parser *parser);
+#endif //PARSER_HPP

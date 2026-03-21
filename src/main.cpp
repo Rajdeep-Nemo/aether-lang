16a12335122670a91a182e7860269dfd68ff13b0
Rajdeep Nemo <rajdeepnemo@gmail.com>
2026-03-21 19:38:18 +0530

    Basic implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include "Lexer/lexer.hpp"
+#include "Parser/parser.hpp"
+#include "Evaluator/evaluator.hpp"
+#include "AST/ast.hpp"
+#include "Utils/arena.hpp"
 #include <format>
 #include <iostream>
+#include <vector>
 
 int main(const int argc, char *argv[]) {
     if (argc == 1) {
@@ -10,7 +15,31 @@ int main(const int argc, char *argv[]) {
         return 1;
     }
     if (argc == 2) {
-        run_file(argv[1]);
+        // First check
+        if (!run_file(argv[1])) {
+            return 1;
+        }
+        // Token storage
+        std::vector<Token> tokens;
+        Token t;
+        do {
+            t = scan_token();
+            tokens.push_back(t);
+        } while (t.type != TokenType::END_OF_FILE);
+        // Arena allocation
+        Arena start_arena;
+        init_arena(&start_arena, 1024 * 1024);
+        // Parser initialization
+        Parser parser;
+        parser.tokens = tokens;
+        parser.current_position = 0;
+        parser.arena = &start_arena;
+        // Execution
+        const ASTNode *root = parse(&parser);
+        const double result = evaluate(root);
+        std::cout << result << "\n";
+        // Arena deallocation
+        free_arena(&start_arena);
         return 0;
     }
     if (argc > 2) {

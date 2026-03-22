e2627b50b2ea05c9cbe86389aa2bcf6dac7ccd6b
Rajdeep Nemo <rajdeepnemo@gmail.com>
2026-03-22 07:46:45 +0530

    Added explicit casting

--- src/Evaluator/evaluator.cpp
+++ src/Evaluator/evaluator.cpp
@@ -1,5 +1,7 @@
 #include "evaluator.hpp"
 #include "ast.hpp"
+
+#include <iostream>
 RuntimeValue evaluate(const ASTNode *node) {
     if (node == nullptr) {
         return RuntimeValue{ValueType::VAL_NIL};
@@ -44,37 +46,112 @@ RuntimeValue evaluate(const ASTNode *node) {
                 r_val = static_cast<double>(right_val.u);
             }
             switch (node->binary_expr.operator_type) {
-                case TokenType::PLUS: {
-                    RuntimeValue add{};
-                    add.type = ValueType::VAL_FLOAT;
-                    add.f = l_val + r_val;
-                    return add;
-                }
-                case TokenType::MINUS: {
-                    RuntimeValue sub{};
-                    sub.type = ValueType::VAL_FLOAT;
-                    sub.f = l_val - r_val;
-                    return sub;
-                }
-                case TokenType::STAR: {
-                    RuntimeValue mul{};
-                    mul.type = ValueType::VAL_FLOAT;
-                    mul.f = l_val * r_val;
-                    return mul;
-                }
-                case TokenType::SLASH: {
-                    RuntimeValue div{};
-                    div.type = ValueType::VAL_FLOAT;
-                    div.f = l_val / r_val;
-                    return div;
-                }
-                default: {
-                    RuntimeValue nil{};
-                    nil.type = ValueType::VAL_NIL;
-                    return nil;
-                }
+            case TokenType::PLUS: {
+                RuntimeValue add{};
+                add.type = ValueType::VAL_FLOAT;
+                add.f = l_val + r_val;
+                return add;
+            }
+            case TokenType::MINUS: {
+                RuntimeValue sub{};
+                sub.type = ValueType::VAL_FLOAT;
+                sub.f = l_val - r_val;
+                return sub;
+            }
+            case TokenType::STAR: {
+                RuntimeValue mul{};
+                mul.type = ValueType::VAL_FLOAT;
+                mul.f = l_val * r_val;
+                return mul;
+            }
+            case TokenType::SLASH: {
+                RuntimeValue div{};
+                div.type = ValueType::VAL_FLOAT;
+                div.f = l_val / r_val;
+                return div;
+            }
+            default: {
+                RuntimeValue nil{};
+                nil.type = ValueType::VAL_NIL;
+                return nil;
+            }
             }
         }
+        if (left_val.type == ValueType::VAL_UINT && right_val.type == ValueType::VAL_UINT) {
+            uint64_t l_val = left_val.u;
+            uint64_t r_val = right_val.u;
+            switch (node->binary_expr.operator_type) {
+            case TokenType::PLUS: {
+                RuntimeValue add{};
+                add.type = ValueType::VAL_UINT;
+                add.u = l_val + r_val;
+                return add;
+            }
+            case TokenType::MINUS: {
+                RuntimeValue sub{};
+                sub.type = ValueType::VAL_UINT;
+                sub.u = l_val - r_val;
+                return sub;
+            }
+            case TokenType::STAR: {
+                RuntimeValue mul{};
+                mul.type = ValueType::VAL_UINT;
+                mul.u = l_val * r_val;
+                return mul;
+            }
+            case TokenType::SLASH: {
+                RuntimeValue div{};
+                div.type = ValueType::VAL_UINT;
+                div.u = l_val / r_val;
+                return div;
+            }
+            default: {
+                RuntimeValue nil{};
+                nil.type = ValueType::VAL_NIL;
+                return nil;
+            }
+            }
+        }
+        if (left_val.type == ValueType::VAL_INT && right_val.type == ValueType::VAL_INT) {
+            int64_t l_val = left_val.i;
+            int64_t r_val = right_val.i;
+            switch (node->binary_expr.operator_type) {
+            case TokenType::PLUS: {
+                RuntimeValue add{};
+                add.type = ValueType::VAL_INT;
+                add.i = l_val + r_val;
+                return add;
+            }
+            case TokenType::MINUS: {
+                RuntimeValue sub{};
+                sub.type = ValueType::VAL_INT;
+                sub.i = l_val - r_val;
+                return sub;
+            }
+            case TokenType::STAR: {
+                RuntimeValue mul{};
+                mul.type = ValueType::VAL_INT;
+                mul.i = l_val * r_val;
+                return mul;
+            }
+            case TokenType::SLASH: {
+                RuntimeValue div{};
+                div.type = ValueType::VAL_INT;
+                div.i = l_val / r_val;
+                return div;
+            }
+            default: {
+                RuntimeValue nil{};
+                nil.type = ValueType::VAL_NIL;
+                return nil;
+            }
+            }
+        }
+        std::cerr << "Runtime Error: Type mismatch. Cannot implicitly mix signed and unsigned integers. Cast explicitly.\n" << std::endl;
+        RuntimeValue nil{};
+        nil.type = ValueType::VAL_NIL;
+        return nil;
     }
     return RuntimeValue{ValueType::VAL_NIL};
 }
+

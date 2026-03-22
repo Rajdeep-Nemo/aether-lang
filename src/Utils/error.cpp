86d62b78ccc361fd784729ae0db46da4d3cc57e4
Rajdeep Nemo <rajdeepnemo@gmail.com>
2026-03-22 19:29:45 +0530

    Explicit, global error message showing implemented.

---
+++ src/Utils/error.cpp
@@ -0,0 +1,18 @@
+#include "error.hpp"
+#include <iostream>
+
+void report_scan_error(const int line, const std::string_view message) {
+    std::cerr << "[Line " << line << "] Lexer Error: " << message << std::endl;
+}
+
+void report_parser_error(const Token& token,const std::string_view message) {
+    if (token.type == TokenType::END_OF_FILE) {
+        std::cerr << "[Line " << token.line << "] Syntax Error at end of file: " << message << std::endl;
+    } else {
+        std::cerr << "[Line " << token.line << "] Syntax Error at '" << token.lexeme << "': " << message << std::endl;
+    }
+}
+
+void report_runtime_error(const std::string_view message) {
+    std::cerr << "Runtime Error: " << message << std::endl;
+}
